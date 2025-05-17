ce6b848fcf0334b05ea7e200944d7746b47bedf8
anujsamdariya07 <anujsamdariya07@gmail.com>
2025-05-17 17:54:52 +0530

    initialize project with Next.js, Tailwind CSS, and Clerk authentication

---
+++ src/app/callback/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// TODO: Implement the auth callback page
+
+export const dynamic = 'force-dynamic';
+
+const AuthCallbackPage = async () => {
+  return <div>AuthCallbackPage</div>;
+};
+
+export default AuthCallbackPage;
